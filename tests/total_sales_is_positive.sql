15c9e2c53a939c9e973089cdc2d831769671cdca
marcsedogalimany@gmail.com <marcsedogalimany@gmail.com>
2023-01-02 12:02:31 +0000

    total sales is positive test

---
+++ tests/total_sales_is_positive.sql
@@ -0,0 +1,18 @@
+with source as (
+
+select * from {{ ref('stg_customers')}}
+),
+
+
+sales_is_positive as (
+
+select
+    orderid,
+    sum(sales) as total_sales
+from {{ ref('stg_customers' )}}
+group by 1
+having not(total_sales >= 0)
+
+)
+
+select * from sales_is_positive
